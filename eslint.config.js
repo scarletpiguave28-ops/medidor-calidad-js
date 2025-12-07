aa86ce8bbfe5d2afe7789cc0340dcfaa4b3048ec
scarletpiguave28-ops <scarletpiguave28@gmail.com>
2025-12-07 17:20:14 -0500

    Add ESLint configuration for JavaScript files

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    files: ["*.js"],
+    rules: {
+      "semi": ["error", "always"],      // exige punto-y-coma
+      "indent": ["error", 2],           // 2 espacios de indentación
+      "space-before-blocks": "error",   // espacio antes de {
+    }
+  }
+];
